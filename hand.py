72dba79a0f25ca348f1dd9a78b3be621937530b3
AJAberdein <ashley@aberdein.co.za>
2019-10-13 08:46:43 +0200

    Hand class

---
+++ hand.py
@@ -0,0 +1,14 @@
+class Hand:
+    "A hand of dominos instance"
+    
+    def __init__(self):
+        self.dominoes = [Domino]
+    
+    def add_domino(self, domino: Domino):
+        "add a domino to this hand"
+        self.dominoes.append({domino.id : domino})
+        
+    def remove_domino(self, domino_id: int):
+        "remove a domino from this hand"
+        del self.dominoes[domino_id]
+        
